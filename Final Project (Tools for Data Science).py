964b7e348a09a56c4e620a563edb033b66ceb886
M-Saad-Khan <123882727+M-Saad-Khan@users.noreply.github.com>
2023-02-06 15:41:23 +0500

    Add files via upload

---
+++ Final Project (Tools for Data Science).py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **Muhammad Saad Khan**
+#   
+#   Data Analayst
+
+# *I am interested in Data Science as a field because I believe that our world is becoming more an more dependant on data as time goes on. There is a huge amount of data that every organization has but is not utilizing to streamline their process and make them more efficient; that is a gap I feel like I can fill*
+
+# ### The code in the next line is supposed to multiply 420 with 69
+
+# In[4]:
+
+
+420*69
+
+
+# Some random websites from the internet and their domain ratings:
+# 
+# |**Website** |**Link**                               |**DR**|
+# |------------|---------------------------------------|------|
+# |1. Pinterest|[Pinterest](https://www.pinterest.com/)|100   |
+# |2. Reddit   |[Reddit](https://www.reddit.com/)      |99    |
+# |3. Tumblr   |[Tumblr](https://www.tumblr.com/)      |98    |
+# 
+# ~~Google Wesites~~
